3479cd2b8afd2411d3edbfaa9ade5ddc7ff9dbe2
khanhdinh02 <khanhdinh02@users.noreply.github.com>
2020-03-13 15:11:11 +0700

    improve security

--- AddElements.js
+++ AddElements.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
     $("body").on("contextmenu",function(e){
         return false;
     });
+	$(".ex-element:first").after('<div id="img-container" style="position: relative; width:100%;"><div id="cover-layer" style="position: absolute; z-index:1; width:100%; height:100%;"></div></div>');
+	$("#img-container").append($("img, img+br"));
 	$(".ex-element:first").append('<p><a href="../"><b>Trang chủ</b></a></p>\n<p>Nhớ ủng hộ nhóm dịch: <a href="https://fb.com/mashimahirovnfc" target="_blank">Mashima Hiro VN FC</a> và <a href="https://fb.com/Black-Clover-VN-Fanpage-1049221435105449" target="_blank">Black Clover VN Fanpage</a></p>');
 	$(".ex-element:last").append('<p>Nhớ ủng hộ nhóm dịch: <a href="https://fb.com/mashimahirovnfc" target="_blank">Mashima Hiro VN FC</a> và <a href="https://fb.com/Black-Clover-VN-Fanpage-1049221435105449" target="_blank">Black Clover VN Fanpage</a></p>\n<p><a href="../"><b>Trang chủ</b></a></p>');
 });
